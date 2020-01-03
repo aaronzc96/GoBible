bef177ae63f166b1a39046d1bbdefd3c6c5e743e
zc <458215370@qq.com>
2020-01-03 08:37:43 +0800

    Initial commit

---
+++ fetch/fetch.go
@@ -0,0 +1,29 @@
+package fetch
+
+import "fmt"
+
+const (
+	_ = 1 << (10 * iota)
+	KiB
+	MiB
+	GiB
+	TiB
+	PiB
+	EiB
+	ZiB
+	YiB
+)
+
+func Zero(ptr [32]byte) {
+	for i := range ptr {
+		fmt.Println(i)
+		ptr[i] = 0
+	}
+}
+
+func One(ptr []byte) {
+	for i := range ptr {
+		fmt.Println(i)
+		ptr[i] = 1
+	}
+}
